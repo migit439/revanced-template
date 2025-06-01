a2843736124b64ec4fd7bae2d35bba8d349c5c34
migit439 <mi.git439@gmail.com>
2025-06-01 23:55:53 +0700

    Update build.gradle.kts

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -4,8 +4,8 @@ patches {
     about {
         name = "ReVanced Patches template"
         description = "Patches template for ReVanced"
-        source = "git@github.com:revanced/revanced-patches-template.git"
-        author = "ReVanced"
+        source = "git@github.com:migit439/revanced-patches.git"
+        author = "Migit439"
         contact = "contact@revanced.app"
         website = "https://revanced.app"
         license = "GNU General Public License v3.0"
